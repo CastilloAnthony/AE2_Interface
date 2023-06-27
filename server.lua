4d80e873f58e6c173754c0e87c79478964f75b32
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2023-06-27 16:20:50 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,29 +7,19 @@ local server = {} -- Stores all of the functions for the server
 
 function server.checkForBridge()
   for _, i in pairs(peripheral.getNames()) do
-    if i == 'meBridge_0' then
-      term.write('Bridge found!')
-      return true
-    elseif (i == 'bottom') or (i == 'top') or (i == 'left') or (i == 'right') or (i == 'front') or (i == 'back') then
       if peripheral.getType(i) == 'meBridge' then
         term.write('Bridge found!')
         return true
       end
     end
   return false
-  end
 end --end checkForBridge
     
 function server.checkForMonitor()
   for _, i in pairs(peripheral.getNames()) do
-    if i == 'monitor' then
+    if peripheral.getType(i) == 'monitor' then
       term.write('Monitor found!')
       return true
-    elseif (i == 'bottom') or (i == 'top') or (i == 'left') or (i == 'right') or (i == 'front') or (i == 'back') then
-      if peripheral.getType(i) == 'monitor' then
-        term.write('Monitor found!')
-        return true
-      end
     end
   end
   return false
@@ -44,23 +34,23 @@ function server.drawData(monitor, items, energy)
   monitor.clear()
   monitor.setCursorPos(0,0)
   monitor.write('Available Storage: ')
-  monitor.write(items[2])
+  monitor.write(items[3])
   monitor.write(' ')
-  monitor.write(items[1]/items[0])
+  monitor.write(items[2]/items[1])
   monitor.write('%')
   monitor.setCursorPos(0, 1)
   monitor.write('Total Stored Items: ')
-  monitor.write(items[1])
+  monitor.write(items[2])
   monitor.write(' out of ')
   monitor.write(items[0])
   monitor.setCursorPos(0,2)
   montior.write('Available Energy: ')
-  monitor.write(energy[0])
+  monitor.write(energy[1])
   monitor.write(' ')
-  monitor.write(energy[0]/energy[1])
+  monitor.write(energy[1]/energy[2])
   monitor.setCursorPos(0,3)
   monitor.write('Current Energy Usage: ')
-  monitor.write(energy[2])
+  monitor.write(energy[3])
 end --end drawData
 
 function server.getEnergyInfo(bridge)
