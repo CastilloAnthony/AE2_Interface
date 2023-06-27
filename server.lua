1685b0ba5705d6a2d14dfd43e0cf16d367227a7b
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2023-06-27 15:21:48 -0700

    Add files via upload

--- server.lua
+++ server.lua
@@ -9,9 +9,80 @@ function server.checkForBridge()
   for i in peripheral.getNames() do
     if i == 'meBridge_0' then
       return true
+    end
+    elseif (i == 'bottom') or (i == 'top') or (i == 'left') or (i == 'right') or (i == 'front') or (i == 'back') then
+      if peripheral.getType(i) == 'meBridge' then
+        return true
+      end
+    end
   return false
+  end
 end --end checkForBridge
     
-function server.main()
-  if server.checkForBridge() then
+function server.checkForMonitor()
+  for i in peripheral.getNames() do
+    if i == 'monitor' then
+      return true
+    end
+    elseif (i == 'bottom') or (i == 'top') or (i == 'left') or (i == 'right') or (i == 'front') or (i == 'back') then
+      if peripheral.getType(i) == 'monitor' then
         return true
+      end
+    end
+  end
+  return false
+end --end checkForMonitor
+
+function server.initializeMonitor()
+  monitor.clear()
+  monitor.setCursorPos(0,0)
+end --end initializeMonitor
+
+function server.drawData(monitor, items, energy)
+  monitor.clear()
+  monitor.setCursorPos(0,0)
+  monitor.write('Available Storage: ')
+  monitor.write(items[2])
+  monitor.write(' ')
+  monitor.write(items[1]/items[0])
+  monitor.write('%')
+  monitor.setCursorPos(0, 1)
+  monitor.write('Total Stored Items: ')
+  monitor.write(items[1])
+  monitor.write(' out of ')
+  monitor.write(items[0])
+  monitor.setCursorPos(0,2)
+  montior.write('Available Energy: ')
+  monitor.write(energy[0])
+  monitor.write(' ')
+  monitor.write(energy[0]/energy[1])
+  monitor.setCursorPos(0,3)
+  monitor.write('Current Energy Usage: ')
+  monitor.write(energy[2])
+end --end drawData
+
+function server.getEnergyInfo(bridge)
+  return [bridge.getEnergyStorage(), bridge.getMaxEnergyStorage(), bridge.getEnergyUsage()]
+end --end getEnergyInfo
+
+function server.getItemStorageInfo(bridge)
+  return [bridge.getTotalItemStorage(), bridge.getUsedItemStorage(), bridge.getAvailableItemStorage()]
+end --end getItemStorageInfo
+
+function server.main()
+  if server.checkForBridge() and server.checkForMonitor() then
+    local bridge = peripheral.find('meBridge')
+    local monitor = peripheral.find('monitor')
+  else
+    return false
+  end
+
+  while true do
+    itemsInfo = server.getItemStorage(bridge)
+    energyInfo = server.getEnergyInfo(bridge)
+    server.drawData(monitor, itemsInfo, energyInfo)
+    os.sleep(1)
+
+end --end main
+
+return server
