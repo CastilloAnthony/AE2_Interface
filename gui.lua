b730d85605d8956b9c1bfe33d27b531743266b66
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2024-07-03 15:06:07 -0700

    Update gui.lua
    Color change

--- gui.lua
+++ gui.lua
@@ -362,11 +362,13 @@ function gui.page2(energyInfo) -- Energy
     gui.monitor.setTextColor(colors.magenta)
     gui.monitor.setCursorPos(gui.width*gui.widthFactor,8)
     gui.monitor.write(math.floor(energyInfo['usage']*2.5)..' '..'RF/t')
+    gui.monitor.setTextColor(colors.purple)
     gui.monitor.setCursorPos(2,9)
     gui.monitor.write('Delta Power: ')
     gui.monitor.setTextColor(colors.magenta)
     gui.monitor.setCursorPos(gui.width*gui.widthFactor,9)
     gui.monitor.write(math.floor(energyInfo['currentStorage']-gui.settings['recentPower'])..' '..'TBD')
+    gui.monitor.setTextColor(colors.purple)
     gui.monitor.setCursorPos(2,10)
     gui.drawButtons()
     gui.settings['recentPower'] = energyInfo['currentStorage']
