c2ee3c6a7a2b981e5180932ce0b3209d325c649a
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2024-07-04 02:17:31 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -361,7 +361,7 @@ function server.initialize()
   server.modem = initial['modem']
   server.initializeMonitor(monitor)
   server.initializeNetwork(modem)
-  parallel.waitForAny(server.guiTime, server.main, server.generateSnapshots, server.eventHandler,)#server.checkCraftingQueue)
+  parallel.waitForAny(server.guiTime, server.main, server.generateSnapshots, server.eventHandler,) --server.checkCraftingQueue)
 end --end initialize
 
 return server
