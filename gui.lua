66b729b1b2919df8ec90ed2d7c7f8a081aacb6af
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2024-07-04 15:14:33 -0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -222,6 +222,11 @@ function gui.clickedButton(button, x, y, craftables)
                     gui.log('Usr Inpt: '..gui.userSearch)
                 end
             end
+        gui.log(x)
+        gui.log(y)
+        gui.log(gui.width)
+        gui.log(gui.widthFacto)
+        gui.log(gui.height)
         elseif 1 < x < gui.width-gui.width*gui.widthFactor and 3 < y < gui.height-2 then
             if gui.settings['currentPage'] == 6 then -- Craftables
                 if x>=2 and x<=gui.width*gui.widthFactor then -- Clicked the name of an item
