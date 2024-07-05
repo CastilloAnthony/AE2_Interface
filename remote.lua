69eb3c2fa68a7e5f38b4409bfe88ab5e311c248a
Anthony C <57550910+CastilloAnthony@users.noreply.github.com>
2024-07-04 17:07:47 -0700

    Update remote.lua

--- remote.lua
+++ remote.lua
@@ -246,7 +246,7 @@ function remote.eventHandler()
                         if arg4['message'] == 'Acknowledged.' then
                             if remote.craftRequest[arg4['packet']['timestamp']] ~= nil then
                                 acknowledged = True
-                                table.remote(remote.craftRequest, arg4['packet']['timestamp'])
+                                table.remove(remote.craftRequest, arg4['packet']['timestamp'])
                             end
                         end
                     end
